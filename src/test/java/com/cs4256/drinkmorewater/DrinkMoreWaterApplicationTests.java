2a9fbbcbd4675bc0fea80294fce140e79acb409b
hzzhang <monsttrod@gmail.com>
2022-10-18 11:28:58 -0400

    setup

---
+++ src/test/java/com/cs4256/drinkmorewater/DrinkMoreWaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs4256.drinkmorewater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkMoreWaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
