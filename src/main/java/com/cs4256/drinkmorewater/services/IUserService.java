04b388a25ca6634212eb67c4cd93ed286b0b3f51
hzzhang <monsttrod@gmail.com>
2022-10-23 16:54:03 -0400

    complete CRUD unit test for User

--- src/main/java/com/cs4256/drinkmorewater/services/IUserService.java
+++ src/main/java/com/cs4256/drinkmorewater/services/IUserService.java
@@ -4,4 +4,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.cs4256.drinkmorewater.models.User;
 
 public interface IUserService extends IService<User> {
+
 }
--- src/test/java/com/cs4256/drinkmorewater/UserTests.java
+++ src/test/java/com/cs4256/drinkmorewater/UserTests.java
@@ -1,9 +1,8 @@
 package com.cs4256.drinkmorewater;
 
-import com.baomidou.mybatisplus.test.autoconfigure.MybatisPlusTest;
 import com.cs4256.drinkmorewater.mapper.UserMapper;
 import com.cs4256.drinkmorewater.models.User;
-import com.cs4256.drinkmorewater.services.IUserService;
+import com.cs4256.drinkmorewater.services.impl.UserServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -12,14 +11,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserTests {
 
     @Autowired
-    private IUserService iUserService;
+    private UserServiceImpl userServiceImpl;
 
     @MockBean
     private UserMapper userMapper;
@@ -32,6 +34,53 @@ public class UserTests {
 
         Mockito.when(userMapper.selectById(testId)).thenReturn(retUser);
 
-        assertEquals(iUserService.getById(testId).getUserId(), testId);
+        assertEquals(userServiceImpl.getById(testId), retUser);
+    }
+
+    @Test
+    public void testGetAll() {
+        List<User> userList = Arrays.asList(
+                new User(1, "John", "Doe", "jd123@gmail.com", "15874678", 1),
+                new User(2, "Jane", "Nieh", "jn123@gmail.com", "15874630", 1));
+
+        Mockito.when(userMapper.selectList(null)).thenReturn(userList);
+
+        assertNotNull(userServiceImpl.list());
+    }
+
+    @Test
+    public void testUpdate() {
+        User updateUser = new User(1, "Jason", "Nieh", "jn123@gmail.com", "15874678", 1);
+
+        Mockito.when(userMapper.updateById(updateUser)).thenReturn(1);
+
+        assertTrue(userServiceImpl.updateById(updateUser));
+    }
+
+    @Test
+    public void testInvalidUpdate() {
+        User updateUser = new User(-1, "Jason", "Nieh", "jn123@gmail.com", "15874678", 1);
+
+        Mockito.when(userMapper.updateById(updateUser)).thenReturn(0);
+
+        assertFalse(userServiceImpl.updateById(updateUser));
+    }
+
+    @Test
+    public void testAddUser() {
+        User userToAdd = new User(1, "Jason", "Nieh", "jn123@gmail.com", "15874678", 1);
+
+        Mockito.when(userMapper.insert(userToAdd)).thenReturn(1);
+
+        assertTrue(userServiceImpl.save(userToAdd));
+    }
+
+    @Test
+    public void testRemoveUser() {
+        int removeId = 1;
+
+        Mockito.when(userMapper.deleteById(removeId)).thenReturn(1);
+
+        assertTrue(userServiceImpl.removeById(removeId));
     }
 }
