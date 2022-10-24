1edb6e1f9a8f0439503a575702576d396b975384
Ruochen Li <rl3292@columbia.edu>
2022-10-24 14:01:23 -0400

    delete serve

--- src/main/java/com/cs4256/drinkmorewater/services/IServeService.java
+++
@@ -1,8 +0,0 @@
-package com.cs4256.drinkmorewater.services;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.cs4256.drinkmorewater.models.Serve;
-
-// TODO: need to dinfine
-public interface IServeService extends IService<Serve>  {
-}
